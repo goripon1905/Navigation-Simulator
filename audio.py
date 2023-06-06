8fe6ebcadbd26ba4bda53c41f17363a3ffb8b0a3
goripon1905 <goripon1905@yahoo.co.jp>
2023-06-06 15:55:53 +0900

    エラー表示追加

--- audio.py
+++ audio.py
@@ -1,8 +1,8 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QVBoxLayout
-from PyQt5.QtGui import QPixmap, QIcon
+from PyQt5.QtGui import QPixmap, QIcon, QFont, QColor
 from PyQt5.QtMultimedia import QSoundEffect
-from PyQt5.QtCore import QUrl, QSize, QTimer
+from PyQt5.QtCore import QUrl, QSize, QTimer, Qt
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -16,14 +16,23 @@ class MainWindow(QMainWindow):
         self.button_click_sound.setSource(QUrl.fromLocalFile("source/pi.wav"))
 
 
-#audio_menu.pngを表示
+        # audio_menu.pngを表示
         bkg = QPushButton(self)
         bkg.setGeometry(0, 0, 700, 400)
         bkg.setStyleSheet("background-color: rgba(0, 0, 0, 0)")
-        bkg.setIcon(QIcon("source/audio_menu.png"))
+        bkg.setIcon(QIcon("source/menu.png"))
         bkg.setIconSize(bkg.size())
         bkg.raise_()
 
+        # メニュー名ラベル
+        label = QLabel("オーディオソース選択", self)
+        label.setGeometry(15, 10, 200, 30)
+        font1 = QFont("Meiryo", 15)
+        label.setFont(font1)
+        color = QColor("aqua")
+        label.setStyleSheet("color: {}".format(color.name()))
+
+        # ボタン配置
         button1 = QPushButton(self)
         button1.setGeometry(110, 100, 112, 112)
         button1.setStyleSheet("background-color: rgba(0, 0, 0, 0)")
@@ -74,10 +83,39 @@ class MainWindow(QMainWindow):
         button7 = QPushButton(self)
         button7.setGeometry(350, 220, 112, 112)
         button7.setStyleSheet("background-color: rgba(0, 0, 0, 0)")
-        button7.clicked.connect(self.button_click)
+        button7.clicked.connect(self.info1_click)
         button7.setIcon(QIcon("source/USB.png"))
         button7.setIconSize(button7.size())
         button7.raise_()
+
+        self.info_label = QLabel(self)
+        self.info_label.setGeometry(0, 0, 700, 400)
+        self.info_label.setScaledContents(True)
+        self.info_label.hide()
+
+        self.text_label = QLabel(self)
+        self.text_label.setGeometry(145, 190, 1000, 20)
+        self.text_label.setText("接続されたUSBデバイスは対応していません")
+        self.font2 = QFont("Meiryo", 15, QFont.Bold)
+        self.text_label.setFont(self.font2)
+        self.text_label.setStyleSheet("color: white")
+        self.text_label.hide()
+
+
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.hide_info_label)
+
+    def info1_click(self):
+        self.button_click_sound.play()
+        self.info_label.setPixmap(QPixmap("source/info1.png"))
+        self.info_label.show()
+        self.text_label.show()
+        self.timer.start(3000)  # 3秒間表示する
+
+    def hide_info_label(self):
+        self.info_label.hide()
+        self.text_label.hide()
+        self.timer.stop()
 
     def button_click(self):
         self.button_click_sound.play()
